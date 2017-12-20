8fac357ea367c34ab01b3afc21cc21f973b8b4fc
Popsor Bogdan Alexandru <popsor.balex@yahoo.com>
2017-12-20 23:02:20 +0200

    From P2

--- 4b.pl
+++ 4b.pl
@@ -1,6 +1,6 @@
-For a heterogeneous list, formed from integer numbers and list of digits, write a predicate to compute the
-sum of all numbers represented as sublists.
-Eg.: [1, [2, 3], 4, 5, [6, 7, 9], 10, 11, [1, 2, 0], 6] => [8, 2, 2].
+%For a heterogeneous list, formed from integer numbers and list of digits, write a predicate to compute the
+%sum of all numbers represented as sublists.
+%Eg.: [1, [2, 3], 4, 5, [6, 7, 9], 10, 11, [1, 2, 0], 6] => [8, 2, 2].
 
 
 %list(L-list,R-list)
