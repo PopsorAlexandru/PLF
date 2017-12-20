5df8b0a0115cf903ca07a76e8da8673f875b316c
Popsor Bogdan Alexandru <popsor.balex@yahoo.com>
2017-12-20 23:03:16 +0200

    Add files via upload

---
+++ test.pl
@@ -0,0 +1,38 @@
+max([],Max,Max).
+
+max([H|T],Max,R):-
+    H > Max,
+    Max1 is H,
+    max(T,Max1,R1),
+    R is R1.
+
+max([H|T],Max,R):-
+    H =< Max,
+    max(T,Max,R).
+
+
+pos([],_,_,[]).
+
+pos([H|T],Max,Pos,[H1|T1]):-
+    Max =:= H,
+    Pos1 is Pos+1,
+    pos(T,Max,Pos1,T1),
+    H1 is Pos.
+
+
+pos([H|T],Max,Pos,R):-
+    Max =\= H,
+    Pos1 is Pos+1,
+    pos(T,Max,Pos1,R),
+
+main([],[]).
+main(L,R):-
+    max(L,0,Max),
+    pos(L,Max,1,R).
+
+
+
+
+
+
+
